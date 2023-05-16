635028d96e77b1809359f13dfbe974495f768c55
Lucas gomes <lucaass1997rp@gmail.com>
2023-05-16 12:30:01 -0300

    commit

--- js/index.js
+++ js/index.js
@@ -20,9 +20,30 @@ const escrever = () => {
 
   } 
 }
-
 escrever()
 
 if (window.SimpleAnime) {
   new SimpleAnime();
 }
+
+function initScrollSuave() {
+
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+    function scrollToSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        });
+    }
+
+    linksInternos.forEach((link) => {
+        link.addEventListener('click', scrollToSection);
+    });
+
+}
+initScrollSuave();
