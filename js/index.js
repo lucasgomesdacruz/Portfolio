018fcb1bf094266a417ddaa6b56adb27e24aab22
Lucas gomes <lucaass1997rp@gmail.com>
2023-05-17 01:20:18 -0300

    commit
    add scroll js

--- js/index.js
+++ js/index.js
@@ -47,3 +47,24 @@ function initScrollSuave() {
 
 }
 initScrollSuave();
+
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll('.js-scroll');
+
+  if(sections.length) {
+      const windowMetade = window.innerHeight * 0.5;
+
+      function animaScroll() {
+          sections.forEach((section) => {
+              const sectionTop = section.getBoundingClientRect().top - windowMetade;
+              if(sectionTop < 0) {
+                  section.classList.add('ativo');
+              }
+          })
+      }
+
+      window.addEventListener('scroll', animaScroll);
+  }
+}
+
+initAnimacaoScroll();
