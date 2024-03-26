f633f718418e49c67eacdf1dc1969363cc6388e4
Lucas Gomes <lucaass1997rp@gmail.com>
2024-03-26 15:10:48 -0300

    Removendo evento js

--- js/index.js
+++ js/index.js
@@ -47,23 +47,3 @@ function initScrollSuave() {
 }
 initScrollSuave();
 
-function initAnimacaoScroll() {
-  const sections = document.querySelectorAll('.js-scroll');
-
-  if(sections.length) {
-      const windowMetade = window.innerHeight * 0.1;
-
-      function animaScroll() {
-          sections.forEach((section) => {
-              const sectionTop = section.getBoundingClientRect().top - windowMetade;
-              if(sectionTop < 0) {
-                  section.classList.add('ativo');
-              }
-          })
-      }
-
-      window.addEventListener('scroll', animaScroll);
-  }
-}
-
-initAnimacaoScroll();
